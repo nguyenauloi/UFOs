dce69770e554d9b1eff034221bd5342f53a6d7ad
Austin Nguyen <originalnguyensta@gmail.com>
2022-09-20 17:52:07 -0600

    11.5.2

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-{
-    datetime: "1/1/2010",
-    city: "benton",
-    state: "ar",
-    country: "us",
-    shape: "circle",
-    durationMinutes: "5 mins.",
-    comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
-  },
-{
-    datetime: "1/1/2010",
-    city: "bontio",
-    state: "ca",
-    country: "us",
-    shape: "light",
-    durationMinutes: "c",
-    comments: "Three bright red lights witnessed floating stationary over San Diego New Years Day 2010"
-  },
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Build the table
+function buildTable(data) {
+  // First, clear out any existing data
+  tbody.html("");
+
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );
+  });
+}
+
+
