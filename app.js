848c7a2ee3cc3fad7168a2beb3edf882bb249f9b
Austin Nguyen <originalnguyensta@gmail.com>
2022-09-20 17:20:07 -0600

    11.5.1

---
+++ app.js
@@ -0,0 +1,18 @@
+{
+    datetime: "1/1/2010",
+    city: "benton",
+    state: "ar",
+    country: "us",
+    shape: "circle",
+    durationMinutes: "5 mins.",
+    comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
+  },
+{
+    datetime: "1/1/2010",
+    city: "bontio",
+    state: "ca",
+    country: "us",
+    shape: "light",
+    durationMinutes: "c",
+    comments: "Three bright red lights witnessed floating stationary over San Diego New Years Day 2010"
+  },
