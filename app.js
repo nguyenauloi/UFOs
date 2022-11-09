d13355d0c74505f5e256bdd1606d04b89f5b0e69
Austin Nguyen <originalnguyensta@gmail.com>
2022-11-09 01:38:21 -0700

    11.5.4

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// 11.2.4
 // import the data from data.js
 const tableData = data;
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
+// 11.5.1
 // Build the table
 function buildTable(data) {
   // First, clear out any existing data
@@ -25,4 +27,29 @@ function buildTable(data) {
   });
 }
 
+// 11.5.3
+// Handleclick start
+function handleClick() {
+  // Grab the datetime value from the filter
+  let date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
 
+   // Check to see if a date was entered and filter the
+  // data using that date.
+  if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  }
+
+   // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
